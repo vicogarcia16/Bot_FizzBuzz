ca3a7fd7b038f541fcc844158bc0295dccb3fbb8
vmgn16 <mg_619@hotmail.com>
2022-05-06 18:12:06 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "jest": true,
+        "node": true
+
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+
+        "no-unused-vars": "off",
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"]
+    }
+};
---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,18 @@
+const ExplorerService = require("./../../lib/services/ExplorerService");
+
+describe("Tests para ExplorerService", () => {
+    test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
+        const explorers = [{mission: "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
+    });
+    test("Requerimiento exploradores filtrados por stack", () => {
+        const explorers = [{ stack: "javascript" }];
+        const explorersInJs = ExplorerService.filterByStackValue(
+          explorers,
+          "javascript"
+        );
+        expect(explorersInJs.length).toBe(1);
+      });
+
+});
